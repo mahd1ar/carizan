83286a3d896fc179e3f7702795d6cecdbccae4f1
mahdiyar <a.mahdiyar7@live.com>
2022-08-14 12:40:28 +0430

    init

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      colors: {
+        tm: {
+          black: '#232E3B',
+          gray: '#626F7F',
+          yellow: { DEFAULT: '#f7ca18', dark: '#67540a' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
